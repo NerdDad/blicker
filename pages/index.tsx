edcc46fb517ebff1801f06fb55dfbc6491eaba1e
Jeremy Cromwell <NerdDad@users.noreply.github.com>
2021-06-26 15:35:51 -0700

    add head tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import Head from "next/head";
+import styles from "../styles/Index.module.css";
+
 export default function Main() {
-  return <div>Hello World</div>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Bible Clicker</title>
+        <meta
+          name="description"
+          content="An incremental game with Bible verses"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Hello World
+    </div>
+  );
 }
